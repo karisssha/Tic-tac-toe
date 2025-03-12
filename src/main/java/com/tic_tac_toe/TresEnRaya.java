4f1258aaeb622e5f46bd735d2caca1e49de70aa0
RieraDiPe <rieradipe@gmail.com>
2025-03-12 15:38:51 +0100

    add title

---
+++ src/main/java/com/tic_tac_toe/TresEnRaya.java
@@ -0,0 +1,12 @@
+package com.tic_tac_toe;
+
+//import java.util.Scanner;
+
+public class TresEnRaya 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bienvenidos al Juego Tres en Raya!" );
+    }
+}
+//close Scanner;
