798f453307513caafa460ae7da815568cf37754f
RieraDiPe <rieradipe@gmail.com>
2025-03-12 16:18:31 +0100

    we start game

--- src/main/java/com/tic_tac_toe/TresEnRaya.java
+++ src/main/java/com/tic_tac_toe/TresEnRaya.java
@@ -1,12 +1,26 @@
 package com.tic_tac_toe;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
-public class TresEnRaya 
-{
-    public static void main( String[] args )
-    {
+public class TresEnRaya {
+    private static char[] board = new char[9];
+    private static Scanner scanner = new Scanner(System.in);
+    private static char player1 = 'X';
+    private static char player2 = 'O';
+
+    
+    public static void main( String[] args ) {
         System.out.println( "Bienvenidos al Juego Tres en Raya!" );
-    }
+    
+    public static void printTheBoard(char[9] 'board');//nos uedamos aqui con este error
+    
+{
+System.out.println( board[0] + " | " + board[1] + " | " + board[2]);
+System.out.println( " - + - + - " );
+System.out.println( board[3] + " | " + board[4] + " | " + board[5]);
+System.out.println( " - + - + - " );
+System.out.println( board[6] + " | " + board[7] + " | " + board[8]);
+scanner.close();
+};
 }
-//close Scanner;
+}
