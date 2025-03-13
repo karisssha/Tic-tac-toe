9aa957744269cc9dd7817e8ddbf0c93c53133872
karisssha <krshmelendez@gmail.com>
2025-03-13 13:25:06 +0100

    updated players with try and catch and a board object

--- src/main/java/com/tic_tac_toe/Players.java
+++ src/main/java/com/tic_tac_toe/Players.java
@@ -1,26 +1,35 @@
-public class Players {
+package main.java.com.tic_tac_toe;
+
+import java.util.Scanner;
+
+public class  Players {
+    private Scanner scanner;
+    private Board board;
+
+    public Players(Board board) {
+        this.board = board;
+        this scanner = new Scanner(System.in);
+
+    }
+
+
+    public int getPlayerMove(){
+        while (true) {
+            System.out.printf("Juagdor %c, elige una posición (1-9): ",
+                board.getCurrentPlayer());
     
-    char currentPlayer = 'X';
+            try{ int position = scanner.nextInt() - 1 ; 
+                if (board.isValidMove(position)){
+                    return position;
+                }System.out.println("Movimiento inválido, intenta de nuevo.");
+            } catch(Exception e){
+                System.out.println("Entrada invalida, por favor ingresa un numero");
+                scanner.nextLine();
+            }
+        }
+    }
 
-    while (movesPlayed < 9) {
-        printBoard(board); 
-        System.out.printf("Juagdor %c, elige una posición (1-9): ", currentPlayer);
-
-        int position = scanner.nextInt() - 1;
-
-        if(position < 0 || position >= 9 || board[position] == "X" || board[position] == "O"){
-            System.out.println(null "Movimiento inválido, intenta de nuevo.");
-        
-            board[position] = currentPlayer;
-        }else if(checkWinner(board, currentPlayer)){
-            printBoard(board); 
-            System.out.println(null "Jugador " + currentPlayer + "gana! Felicidades!!");
-
-            movesPlayed++;
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }else if(movesPlayed == 9){
-            printBoard(board);
-            System.out.println(null"Empate!");
-        };
-}
+    public void switchPlayer(){
+        board.switchPlayer(); 
+    }
 }
--- src/main/java/com/tic_tac_toe/TicTacToe.java
+++ src/main/java/com/tic_tac_toe/TicTacToe.java
@@ -1,20 +1,12 @@
 package com.tic_tac_toe;
 
-import java.util.Scanner;
-
 public class TicTacToe {
     public static void main(String[] args) {
-        char[] board = {'1','2','3','4','5','6','7','8','9'};
-        int movesPlayed = 0;
-        char currentPlayer = 'X';
-        Scanner scanner = new Scanner(System.in);
-
-
-        
-            scanner.close();
-        }
+        Board board = new Board();
+        Players game = new Players();
+        game.playGame();
     }
-
+}
 
 
    
