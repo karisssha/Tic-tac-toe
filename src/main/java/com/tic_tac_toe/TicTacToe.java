aa44201f0efdd7970b65b0074ae8fa9d3423e483
karisssha <krshmelendez@gmail.com>
2025-03-13 10:30:19 +0100

    created movements for players x and o

--- src/main/java/com/tic_tac_toe/TicTacToe.java
+++ src/main/java/com/tic_tac_toe/TicTacToe.java
@@ -8,5 +8,32 @@ public class TicTacToe {
         int movesPlayed = 0;
         char currentPlayer = 'X';
         Scanner scanner = new Scanner(System.in);
+
+
+        while (movesPlayed < 9) {
+            printBoard(board); 
+            System.out.printf("Juagdor %c, elige una posición (1-9): ", currentPlayer);
+
+            int position = scanner.nextInt() - 1;
+
+            if(position < 0 || position >= 9 || board[position] == "X" || board[position] == "O"){
+                System.out.println(null "Movimiento inválido, intenta de nuevo.");
+            
+                board[position] = currentPlayer;
+            }else if(checkWinner(board, currentPlayer)){
+                printBoard(board); 
+                System.out.println(null "Jugador " + currentPlayer + "gana! Felicidades!!");
+
+                movesPlayed++;
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }else if(movesPlayed == 9){
+                printBoard(board);
+                System.out.println(null"Empate!");
+            };
+            scanner.close();
+        }
     }
+}
+
+
    
