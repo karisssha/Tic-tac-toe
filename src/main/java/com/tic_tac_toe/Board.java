97b3abde4e0a925ecff1ea4bc977c64194ffa3f0
karisssha <krshmelendez@gmail.com>
2025-03-14 16:46:13 +0100

    updated files with adding colors to the symbols and board

--- src/main/java/com/tic_tac_toe/Board.java
+++ src/main/java/com/tic_tac_toe/Board.java
@@ -4,11 +4,16 @@ public class Board {
     private char[] board;
     private int movesPlayed;
     private char currentPlayer;
+    private final String purple = "\033[35m";
+    private final String green = "\033[32m";
+    private final String reset = "\u001B[0m";
+    
 
     public Board() {
         board = new char[9];
         movesPlayed = 0;
         currentPlayer = 'X';
+        
 
         for(int i = 0; i < 9; i ++) {
             board[i] = (char)('1' + i);
@@ -19,13 +24,23 @@ public class Board {
     }
     public void printBoard() {
         System.out.println();
-        System.out.printf(" %s | %s | %s %n", board[0], board[1], board[2]);
+        System.out.printf(" %s | %s | %s %n", formatSymbol(board[0]), formatSymbol(board[1]), formatSymbol(board[2]));
         System.out.println("---+---+---");
-        System.out.printf(" %s | %s | %s %n", board[3], board[4], board[5]);
+        System.out.printf(" %s | %s | %s %n", formatSymbol(board[3]), formatSymbol(board[4]), formatSymbol(board[5]));
         System.out.println("---+---+---");
-        System.out.printf(" %s | %s | %s %n", board[6], board[7], board[8]);
+        System.out.printf(" %s | %s | %s %n", formatSymbol(board[6]), formatSymbol(board[7]), formatSymbol(board[8]));
         System.out.println();
     }
+
+    private String formatSymbol(char symbol) {
+        if (symbol == 'X') {
+            return green + symbol + reset;
+        } else if (symbol == 'O') {
+            return purple + symbol + reset;
+        }
+        return String.valueOf(symbol);
+    }
+
     public boolean isValidMove(int position) {
         return position >= 0 && position < 9 && 
                (board[position] == '1' || board[position] == '2' || 
